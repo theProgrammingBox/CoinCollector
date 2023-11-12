8719de0bce310419f6e83c09898eebce9da3b4c4
theProgrammingBox <justinboxgea@gmail.com>
2023-11-12 14:40:10 -0800

    testing colors. trying better noise range next

--- Source.c
+++ Source.c
@@ -15,6 +15,22 @@ double seamlessNoise2D(const struct osn_context *ctx, double x, double y, double
     return open_simplex_noise4(ctx, nx, ny, nz, nw);
 }
 
+double octaveNoise2D(const struct osn_context *ctx, double x, double y, double gridWidth, double gridHeight, float frequency, int octaves, float persistence) {
+    double total = 0;
+    double frequencyAccumulator = frequency;
+    double amplitudeAccumulator = 1;
+    double maxValue = 0;
+
+    for (int i = 0; i < octaves; i++) {
+        total += seamlessNoise2D(ctx, x, y, gridWidth, gridHeight, frequencyAccumulator) * amplitudeAccumulator;
+        maxValue += amplitudeAccumulator;
+        amplitudeAccumulator *= persistence;
+        frequencyAccumulator *= 2;
+    }
+
+    return total / maxValue;
+}
+
 uint16_t rand16(uint32_t* seed) {
     *seed ^= *seed >> 16;
     *seed *= 0xBAC57D37;
@@ -27,7 +43,7 @@ int main() {
     const uint8_t VIEW_RADIUS = 16;
     const uint8_t VIEW_SIZE = VIEW_RADIUS * 2 + 1;
 
-    uint32_t seed = time(NULL);
+    uint32_t seed = time(NULL) ^ 0x5EED5EED;
     for (int i = 16; i--;) rand16(&seed);
     struct osn_context *ctx;
     open_simplex_noise(seed, &ctx);
@@ -39,40 +55,58 @@ int main() {
     for (uint32_t i = 0x10000; i--;) {
         double x = (i & 0xFF);
         double y = (i >> 8);
-        grid[i] = (seamlessNoise1D(ctx, x, y, 0x100, 0x100, 8) + 1) * 4;
+        // grid[i] = (seamlessNoise2D(ctx, x, y, 0x100, 0x100, 4) + 1) * 4;
+        grid[i] = (octaveNoise2D(ctx, x, y, 0x100, 0x100, 4, 4, 0.5) + 1) * 4;
     }
-    grid[0] = 1;
     
-    // while(1) {
+    while(1) {
         system("clear");
         for (uint8_t i = VIEW_SIZE, ry = y + VIEW_RADIUS; i--; ry--) {
             for (uint8_t j = VIEW_SIZE, rx = x + VIEW_RADIUS; j--; rx--) {
                 switch (grid[ry << 8 | rx]) {
-                
-                case 0: printf("\x1b[38;2;000;000;128m..\x1b[0m"); break; // Deep Water (Dark Blue)
-                case 1: printf("\x1b[38;2;135;206;235m--\x1b[0m"); break; // Shallow Water (Sky Blue)
-                case 2: printf("\x1b[38;2;210;180;140m;;\x1b[0m"); break; // Sand (Tan)
-                case 3: printf("\x1b[38;2;000;140;000m==\x1b[0m"); break; // Grass (Green)
-                case 4: printf("\x1b[38;2;000;080;000m++\x1b[0m"); break; // Forest (Darker Green)
-                case 5: printf("\x1b[38;2;139;069;019m**\x1b[0m"); break; // Dirt or Earth (Brown)
-                case 6: printf("\x1b[38;2;128;128;128m##\x1b[0m"); break; // Rocky Terrain (Gray)
-                case 7: printf("\x1b[38;2;255;255;255m@@\x1b[0m"); break; // Snow or High Altitude (White)
+                    case 0: printf("\x1b[38;2;000;000;139m..\x1b[0m"); break;
+                    case 1: printf("\x1b[38;2;000;105;148m--\x1b[0m"); break;
+                    case 2: printf("\x1b[38;2;173;216;230m;;\x1b[0m"); break;
+                    case 3: printf("\x1b[38;2;194;178;128m==\x1b[0m"); break;
+                    case 4: printf("\x1b[38;2;155;118;083m**\x1b[0m"); break;
+                    case 5: printf("\x1b[38;2;100;200;100m++\x1b[0m"); break;
+                    case 6: printf("\x1b[38;2;010;120;010m##\x1b[0m"); break;
+                    case 7: printf("\x1b[38;2;000;080;000m@@\x1b[0m"); break;
                 }
+                // switch (grid[ry << 8 | rx] | ((i & 1) << 3)) {
+                //     case 0: printf("\x1b[38;2;000;000;139m░ \x1b[0m"); break;
+                //     case 1: printf("\x1b[38;2;000;105;148m░░\x1b[0m"); break;
+                //     case 2: printf("\x1b[38;2;173;216;230m▒░\x1b[0m"); break;
+                //     case 3: printf("\x1b[38;2;194;178;128m▒▒\x1b[0m"); break;
+                //     case 4: printf("\x1b[38;2;155;118;083m▓▒\x1b[0m"); break;
+                //     case 5: printf("\x1b[38;2;100;200;100m▓▓\x1b[0m"); break;
+                //     case 6: printf("\x1b[38;2;010;120;010m█▓\x1b[0m"); break;
+                //     case 7: printf("\x1b[38;2;000;080;000m██\x1b[0m"); break;
+                    
+                //     case 8: printf("\x1b[38;2;000;000;139m ░\x1b[0m"); break;
+                //     case 9: printf("\x1b[38;2;000;105;148m░░\x1b[0m"); break;
+                //     case 10: printf("\x1b[38;2;173;216;230m░▒\x1b[0m"); break;
+                //     case 11: printf("\x1b[38;2;194;178;128m▒▒\x1b[0m"); break;
+                //     case 12: printf("\x1b[38;2;155;118;083m▒▓\x1b[0m"); break;
+                //     case 13: printf("\x1b[38;2;100;200;100m▓▓\x1b[0m"); break;
+                //     case 14: printf("\x1b[38;2;010;120;010m▓█\x1b[0m"); break;
+                //     case 15: printf("\x1b[38;2;000;080;000m██\x1b[0m"); break;
+                // }
             }
             printf("\n");
         }
-        
-        // printf("Move (wasd): ");
-        // scanf(" %c", &move);
+
+        printf("Move (wasd): ");
+        scanf(" %c", &move);
         
         // x++;
         // y++;
         // usleep(80000);
 
-        // x += (move == 'a') - (move == 'd');
-        // y += (move == 'w') - (move == 's');
+        x += ((move == 'a') - (move == 'd')) * 16;
+        y += ((move == 'w') - (move == 's')) * 16;
         pidx = y << 8 | x;
-    // }
+    }
     
     free(grid);
     return 0;
